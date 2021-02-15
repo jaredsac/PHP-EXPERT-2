68ac226eb0ad09d7a3f79979271d00d23d5e5dd6
Jared Sackey <jared.sackey@student.rocva.nl>
2021-02-15 12:59:44 +0100

    af

--- opdracht/Fietsendelete.php
+++ opdracht/Fietsendelete.php
@@ -3,7 +3,6 @@ require 'database.php';
 
 
 $id=$_GET['id'];
-var_dump($_GET);
 //VERWIJDER EEN WAARDE UIT EEN DATABASE TABEL
 $sql = "DELETE FROM fietsen WHERE FietsID = :ph_FietsID";
 $stmt = $db_conn->prepare($sql); //stuur naar mysql.
